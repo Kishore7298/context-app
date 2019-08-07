810337804dd758a719cab69765c81ace66a70adc
Kishore S <kishore@switchon.io>
2019-08-08 01:24:51 +0530

    A simple implementation of Context api
    a simple implementation of context to pass the
    message from the parent to grandchild.
    
    Signed-off-by: Kishore S <kishoreas7298@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//first we'll create our first context.
+const MyContext = React.createContext();
+
+//second we'll create our provider
+class MyProvider extends Component {
+  state = { 
+    name:"kishore",
+    age:"21",
+    cool:true
+  }
+  render(){
+    return (
+      <MyContext.Provider value={{state:this.state}}>
+        { this.props.children }
+      </MyContext.Provider>
+    )
+  }
 }
 
+const Person = (props)=>(
+  <MyContext.Consumer >
+    {(context)=>(
+      <div>
+        hey this is also {context.state.name}
+      </div>
+    )}
+  </MyContext.Consumer>
+)
+
+const Family = (props)=>(
+  <div>
+    <Person />
+  </div>
+)
+
+class App extends Component {
+  
+  render() { 
+    return ( 
+      <div>
+        <MyProvider>
+          <Family />
+        </MyProvider>
+      </div>
+     );
+  }
+}
+ 
 export default App;
